471d751f90069084006a4e5f3dc9d4d9659dc757
RaniKamna <kamna.a139@gmail.com>
2022-02-15 20:12:54 +0530

    searching functionality

--- frontend/src/components/Component1.jsx
+++ frontend/src/components/Component1.jsx
@@ -1,23 +1,47 @@
 import '../App.css';
-import axios from "axios";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const Searchbar = () => {
-    // const [value, setValue] = useState('');
+    const [data, setData] = useState([]);
+    const [text, setText] = useState("")
 
-    // const handleChange = (e) => {
-    //     const value = e.target;
-    //     setValue(value);
-    //     //handleSearch(value);
-    // };
+    useEffect(() => {
+        fetch(`http://localhost:2342/products`)
+            .then(x => x.json())
+            .then((d) => {
+                //console.log(d.item);
+                setData(d.item);
+            })
+    }, []);
 
-    //const handleSearch = (value) => axios.get(`${value}`);
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    const handleClick = () => {
+        setData(
+            data.filter((item) => {
+                return item.brandname == text
+            })
+        )
+        setText("")
+    }
 
     return (
-        <div className='searchdiv'>
-            <input type="search" name="" id="searchbar" placeholder='Search Products' /> 
-            {/* //value={value} onChange={handleChange} /> */}
-            <button>Enter</button>
-        </div>
+        <>
+            <div className='searchdiv'>
+                <input type="search" id="searchbar" placeholder='Search Products' value={text} onChange={handleChange} />
+                <button onClick={handleClick}>Enter</button>
+            </div>
+            {data.length !== 0 && <div className='result'>
+                {data.map((item) => (
+                    <div key={item.id}>
+                        <div>
+                            <p>{item.brandname}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>}
+        </>
     )
 }
