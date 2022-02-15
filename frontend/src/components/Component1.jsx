5880b268445e75334298a39dbe3697c3c4775b36
RaniKamna <kamna.a139@gmail.com>
2022-02-15 15:42:09 +0530

    scrolling component2

--- frontend/src/components/Component1.jsx
+++ frontend/src/components/Component1.jsx
@@ -3,19 +3,20 @@ import axios from "axios";
 import { useState } from 'react';
 
 export const Searchbar = () => {
-    const [value, setValue] = useState('');
+    // const [value, setValue] = useState('');
 
-    const handleChange = (e) => {
-        const value = e.target;
-        setValue(value);
-        handleSearch(value);
-    };
+    // const handleChange = (e) => {
+    //     const value = e.target;
+    //     setValue(value);
+    //     //handleSearch(value);
+    // };
 
-    const handleSearch = (value) => axios.get(`${value}`);
+    //const handleSearch = (value) => axios.get(`${value}`);
 
     return (
         <div className='searchdiv'>
-            <input type="search" name="" id="searchbar" placeholder='Search Products' value={value} onChange={handleChange} />
+            <input type="search" name="" id="searchbar" placeholder='Search Products' /> 
+            {/* //value={value} onChange={handleChange} /> */}
             <button>Enter</button>
         </div>
     )
--- frontend/src/components/Component2.jsx
+++ frontend/src/components/Component2.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react'
 import '../App.css'
 
 export const Carousel = () => {
+    const [carou, setCarou] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:2342/carousels`)
+            .then(x => x.json())
+            .then((d) => {
+                //console.log(d.item);
+                setCarou(d.item);
+            })
+    }, []);
+
+    const length = carou.length;
+
+    const next = () => {
+        setCarou(carou === length - 1 ? 0 : carou + 1)
+    }
+
+    const prev = () => {
+        setCarou(carou === 0 ? length - 1 : carou - 1)
+    }
+
     return (
         <>
             <div className='slide'>
-                <button className='prevbtn'>prev</button>
-                <button className='nextbtn'>next</button>
+                {carou.map((e, i) => (
+                    <img key={i} src={`${e.images}`} />
+                ))}
+                {/* <button className='prevbtn'>prev</button> */}
+                {/* <button className='nextbtn'>next</button> */}
             </div>
         </>
     )
